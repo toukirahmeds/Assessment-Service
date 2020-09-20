05b82ecca23b61432154c64386dac3e3c8efa817
Toukir Ahmed <toukirahmeds@gmail.com>
2020-09-20 21:14:20 +0600

    Updated the db helper

--- src/utils/db-helper.ts
+++ src/utils/db-helper.ts
@@ -1,6 +1,7 @@
 import { create } from "domain";
 import {createConnection} from "typeorm";
 
+// Get the newly created db connection.
 export const getDbConnection = async () => {
     return await createConnection();
 }
